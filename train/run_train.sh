60cf0af0223a1ab6cdada593cf537ea0d0bbb5db
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2024-02-12 19:55:18 -0300

    Add notebook to plot train output

--- train/run_train.sh
+++ train/run_train.sh
@@ -1,3 +1,2 @@
 cd /home/fourier/paulo/sam-hq/train
-date > start_date.txt
-nohup python -m torch.distributed.launch --nproc_per_node=1 train.py --checkpoint ../pretrained_checkpoint/sam_vit_b_01ec64.pth --model-type vit_b --output work_dirs/hq_sam_b_001_300 --local_rank 0 --batch_size_train 2 > train_output.txt 2>&1 &
+nohup python -m torch.distributed.launch --nproc_per_node=1 train.py --checkpoint ../pretrained_checkpoint/sam_vit_b_01ec64.pth --model-type vit_b --output work_dirs/hq_sam_b_001_300 --local_rank 0 --batch_size_train 2 > logs/test_train_output.txt 2>&1 &
